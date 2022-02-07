507754a64dfbfbcc6ead3467c45491df2843184f
Macmiller <macamagolf@gmail.com>
2022-02-07 01:01:01 -0300

    Estrutura do projeto

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -7,20 +7,20 @@ import { turnUserAdminController } from "../modules/users/useCases/turnUserAdmin
 
 const usersRoutes = Router();
 
-usersRoutes.post("/", (request, response) =>
+usersRoutes.post("/", (request, response) => {
   createUserController.handle(request, response)
-);
+});
 
-usersRoutes.patch("/:user_id/admin", (request, response) =>
+usersRoutes.patch("/:user_id/admin", (request, response) => {
   turnUserAdminController.handle(request, response)
-);
+});
 
-usersRoutes.get("/:user_id", (request, response) =>
+usersRoutes.get("/:user_id", (request, response) => {
   showUserProfileController.handle(request, response)
-);
+});
 
-usersRoutes.get("/", (request, response) =>
+usersRoutes.get("/", (request, response) => {
   listAllUsersController.handle(request, response)
-);
+});
 
 export { usersRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(8080, () => console.log("Server is running!"));
